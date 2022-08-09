955656f9aab0bff43999b218374ec993fdda2f23
Érico Andrei <ericof@gmail.com>
2022-08-09 16:54:36 -0300

    chore: Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+<div align="center"><img alt="logo" src="./docs/icon.png" width="70" /></div>
+
+<h1 align="center">DSGov para Plone</h1>
+
+Tema visual para Volto baseado no DSGov
+
+## Contribute
+
+- [Issue Tracker](https://github.com/plonegovbr/volto-dsgov-theme/issues)
+- [Source Code](https://github.com/plonegovbr/volto-dsgov-theme/)
+
+## Credits
+
+The development of this add on was sponsored by the Brazilian Plone Community
+
+[![PloneGov-Br](docs/plonegovbr.png)](https://plone.org.br/)
+
+## License
+
+The project is licensed under [MIT](./LICENSE).
---
+++ src/components/SocialNetworks/SocialNetworks.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SocialNetwork from './SocialNetwork';
+
+const SocialNetworks = (props) => {
+  const { title, networks } = props;
+  return (
+    <div className={'col social-network'}>
+      <p className="text-up-01 text-extra-bold text-uppercase">{ title }</p>
+      {networks.map(function (network, i) {
+        return (
+          <SocialNetwork key={i} id={network.id} href={network.href} />
+        );
+      })}
+    </div>
+  );
+};
+
+/**
+ * Property types.
+ * @property {Object} propTypes Property types.
+ * @static
+ */
+SocialNetworks.propTypes = {
+  title: PropTypes.string.isRequired,
+  networks: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      href: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
+/**
+ * Default properties.
+ * @property {Object} defaultProps Default properties.
+ * @static
+ */
+SocialNetworks.defaultProps = {
+  title: "Redes Sociais"
+};
+
+export default SocialNetworks;
